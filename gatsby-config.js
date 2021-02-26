2798bee28b792d0c3ebf2dc6c42a14fd3f4f7aae
Moritz Wright <me@moritzwright.com>
2021-02-26 13:48:46 -0600

    boilerplate done

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Boilerplate Gatsby Netlify CMS`,
+    description: `A boilerplate for Netlify CMS`,
+    author: `@m-wri`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -32,31 +32,30 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `boilerplate-gatsby-netlify-cms`,
+        short_name: `boilerplate`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-gatsby-cloud`,
     // Installed Plugins
-    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          "gatsby-remark-relative-images",
+          `gatsby-remark-relative-images`,
           {
             resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
+            options: {},
           },
         ],
       },
     },
+    `gatsby-plugin-netlify-cms`,
   ],
 }
