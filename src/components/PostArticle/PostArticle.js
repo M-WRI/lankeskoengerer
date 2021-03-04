30d3e507e4c8addadedb3a053bdfdfddbb1c0737
Moritz Wright <me@moritzwright.com>
2021-03-04 16:49:02 -0600

    horizontal scrolling done

--- src/components/PostArticle/PostArticle.js
+++ src/components/PostArticle/PostArticle.js
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+
+import HorizontalScroll from "../HorizontalScroll/HorizontalScroll"
 
 // Styles
 import "./PostArticle.scss"
@@ -33,48 +35,51 @@ const PostArticle = () => {
   `)
 
   return (
-    <div className="post-container">
-      {query.allMarkdownRemark.edges.map(edge => {
-        const { title, mainImages } = edge.node.frontmatter
-        const { html, fields } = edge.node
+    <HorizontalScroll>
+      <div className="post-container">
+        {query.allMarkdownRemark.edges.map(edge => {
+          const { title, mainImages } = edge.node.frontmatter
+          const { html, fields } = edge.node
 
-        // Dynamic width for Article, depending of image ratio
-        let reduceWidth = 0
-        for (let i = 0; i < mainImages.length; i++) {
-          // width * fixed height + margin
-          reduceWidth +=
-            mainImages[i].childImageSharp.fluid.aspectRatio * 550 + 40
-        }
+          // Dynamic width for Article, depending of image ratio
+          let reduceWidth = 0
+          for (let i = 0; i < mainImages.length; i++) {
+            // width * fixed height + margin
+            reduceWidth +=
+              mainImages[i].childImageSharp.fluid.aspectRatio * 550 + 40
+          }
 
-        return (
-          <article key={fields.slug} style={{ width: `${reduceWidth}px` }}>
-            <div className="image-gallery-container">
-              <ul>
-                {mainImages.map(image => {
-                  let imageWidth = image.childImageSharp.fluid.aspectRatio * 550
-                  return (
-                    <li
-                      className="img-wrapper"
-                      style={{ width: `${imageWidth}px` }}
-                    >
-                      <Img
-                        fluid={{
-                          ...image.childImageSharp.fluid,
-                        }}
-                      />
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-            <div
-              className="post-content"
-              dangerouslySetInnerHTML={{ __html: html }}
-            />
-          </article>
-        )
-      })}
-    </div>
+          return (
+            <article key={fields.slug} style={{ width: `${reduceWidth}px` }}>
+              <div className="image-gallery-container">
+                <ul>
+                  {mainImages.map(image => {
+                    let imageWidth =
+                      image.childImageSharp.fluid.aspectRatio * 550
+                    return (
+                      <li
+                        className="img-wrapper"
+                        style={{ width: `${imageWidth}px` }}
+                      >
+                        <Img
+                          fluid={{
+                            ...image.childImageSharp.fluid,
+                          }}
+                        />
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+              <div
+                className="post-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
+            </article>
+          )
+        })}
+      </div>
+    </HorizontalScroll>
   )
 }
 
