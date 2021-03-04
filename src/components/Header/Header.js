28ea7958915f483b5c803e0db21d960dcc8c9170
Moritz Wright <me@moritzwright.com>
2021-03-04 14:44:40 -0600

    implemented header icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,24 @@ import React from "react"
 
 import "./Header.scss"
 
+// Icons
+import Logo from "../../images/logo.svg"
+import Arrow from "../../images/arrow.svg"
+
 const Header = () => {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <header className="header-container">
+      <nav className="header-navigation">
+        <ul>
+          <li>
+            <img src={Logo} alt="Logo" />
+          </li>
+          <li>
+            <img src={Arrow} alt="Über uns" />
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
