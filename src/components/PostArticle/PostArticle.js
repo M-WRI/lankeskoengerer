8c5844f888230027fe783bb5bc5515bad34e6018
Moritz Wright <me@moritzwright.com>
2021-03-05 11:56:42 -0600

    screen to mobile switch

--- src/components/PostArticle/PostArticle.js
+++ src/components/PostArticle/PostArticle.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect } from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
@@ -7,7 +7,26 @@ import HorizontalScroll from "../HorizontalScroll/HorizontalScroll"
 // Styles
 import "./PostArticle.scss"
 
+// Hook
+const useWindowSize = () => {
+  const [size, setSize] = useState([window.innerHeight, window.innerWidth])
+
+  useEffect(() => {
+    const handleRezise = () => {
+      setSize([window.innerHeight, window.innerWidth])
+    }
+
+    window.addEventListener("resize", handleRezise)
+  }, [])
+
+  return size
+}
+
 const PostArticle = () => {
+  const [height, width] = useWindowSize()
+
+  console.log(width, "<---- Width")
+
   const query = useStaticQuery(graphql`
     {
       allMarkdownRemark {
@@ -34,9 +53,11 @@ const PostArticle = () => {
     }
   `)
 
-  return (
-    <HorizontalScroll>
-      <div className="post-container">
+  const postContainer = () => {
+    return (
+      <div
+        className={width <= 600 ? "post-container-mobile" : "post-container"}
+      >
         {query.allMarkdownRemark.edges.map(edge => {
           const { title, mainImages } = edge.node.frontmatter
           const { html, fields } = edge.node
@@ -50,7 +71,10 @@ const PostArticle = () => {
           }
 
           return (
-            <article key={fields.slug} style={{ width: `${reduceWidth}px` }}>
+            <article
+              key={fields.slug}
+              style={width <= 600 ? null : { width: `${reduceWidth}px` }}
+            >
               <div className="image-gallery-container">
                 <ul>
                   {mainImages.map(image => {
@@ -59,7 +83,9 @@ const PostArticle = () => {
                     return (
                       <li
                         className="img-wrapper"
-                        style={{ width: `${imageWidth}px` }}
+                        style={
+                          width <= 600 ? null : { width: `${imageWidth}px` }
+                        }
                       >
                         <Img
                           fluid={{
@@ -79,8 +105,44 @@ const PostArticle = () => {
           )
         })}
       </div>
-    </HorizontalScroll>
-  )
+    )
+  }
+
+  return <HorizontalScroll>{postContainer()}</HorizontalScroll>
 }
 
 export default PostArticle
+
+{
+  /* <div className="post-container">
+        {query.allMarkdownRemark.edges.map(edge => {
+          const { title, mainImages } = edge.node.frontmatter
+          const { html, fields } = edge.node
+
+          return (
+            <article key={fields.slug}>
+              <div className="image-gallery-container">
+                <ul>
+                  {mainImages.map(image => {
+                    return (
+                      <li className="img-wrapper">
+                        <Img
+                          fluid={{
+                            ...image.childImageSharp.fluid,
+                          }}
+                          objectFit="contain"
+                        />
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+              <div
+                className="post-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
+            </article>
+          )
+        })}
+      </div> */
+}
