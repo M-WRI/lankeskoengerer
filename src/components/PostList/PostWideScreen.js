6267ef614f2153b7e829ea13ad832a4f9a6c7f4f
Moritz Wright <me@moritzwright.com>
2021-04-13 10:19:00 -0600

    changes done starting slider

--- src/components/PostList/PostWideScreen.js
+++ src/components/PostList/PostWideScreen.js
@@ -1,18 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import Img from "gatsby-image"
+// import { Link } from "react-scroll"
+import { Link } from "gatsby"
+import styled from "styled-components"
 
 import HorizontalScroll from "react-scroll-horizontal"
 
+const HorizontalScrollComponent = styled(HorizontalScroll)`
+  div:first-child {
+    
+  }
+`
+
 const PostWideScreen = ({ dynamicHeight, data }) => {
+  const [position, setPosition] = useState(1)
+
+  const handlePosition = () => {
+    setPosition(position + 1)
+  }
+
+  console.log(position, "<------- POSITION")
+
   return (
     <>
       <HorizontalScroll config={{ stiffness: 2000, damping: 80 }}>
-        {data.map(edge => {
+        {data.map((edge, i) => {
           const { frontmatter, id } = edge.node
           const { galleria } = frontmatter
 
           return (
-            <div key={id} className="post-wrapper">
+            <div id={`position-${i}`} key={id} className="post-wrapper">
               <div className="image-gallery">
                 <ul>
                   {galleria.map(image => {
@@ -39,6 +56,14 @@ const PostWideScreen = ({ dynamicHeight, data }) => {
           )
         })}
       </HorizontalScroll>
+      <Link
+        // activeClass="active"
+        to={`#position-${position}`}
+        // spy={true}
+        // duration={500}
+      >
+        <div onClick={handlePosition} className="arrow-class"></div>
+      </Link>
     </>
   )
 }
