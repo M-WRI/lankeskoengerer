8bca63eb93f8118d212fd7224af3af97a7dcbce9
Moritz Wright <me@moritzwright.com>
2021-04-17 09:14:23 -0600

    mobile wide done

--- src/components/PostList/PostMobileWideScreen.js
+++ src/components/PostList/PostMobileWideScreen.js
@@ -5,51 +5,55 @@ import HorizontalScroll from "react-scroll-horizontal"
 
 const PostMobileWideScreen = ({ dynamicHeight, data }) => {
   return (
-    <>
-      <HorizontalScroll config={{ stiffness: 150, damping: 25 }}>
-        {data.map(edge => {
-          const { frontmatter, id } = edge.node
-          const { galleria } = frontmatter
+    <div className="wheel-element">
+      <div className="gallery-wrapper">
+        <div className="gallery-container">
+          {/* <HorizontalScroll config={{ stiffness: 150, damping: 25 }}> */}
+          {data.map(edge => {
+            const { frontmatter, id } = edge.node
+            const { galleria } = frontmatter
 
-          return (
-            <div key={id} className="post-wrapper">
-              <div className="image-gallery">
-                <ul>
-                  {galleria.map(image => {
-                    const imgRatio =
-                      image.imgSrc.childImageSharp.fluid.aspectRatio
-                    const dynamicWidth = imgRatio * dynamicHeight
+            return (
+              <div key={id} className="post-wrapper">
+                <div className="image-gallery">
+                  <ul>
+                    {galleria.map(image => {
+                      const imgRatio =
+                        image.imgSrc.childImageSharp.fluid.aspectRatio
+                      const dynamicWidth = imgRatio * dynamicHeight
 
-                    let wideMobileWidth = !image.imgDesc
-                      ? dynamicWidth
-                      : dynamicWidth + 300
+                      let wideMobileWidth = !image.imgDesc
+                        ? dynamicWidth
+                        : dynamicWidth + 300
 
-                    return (
-                      <li
-                        key={image.imgSrc.childImageSharp.id}
-                        style={{ width: wideMobileWidth }}
-                      >
-                        <Img
-                          fluid={image.imgSrc.childImageSharp.fluid}
-                          style={
-                            !image.imgDesc
-                              ? { height: dynamicHeight, width: "100%" }
-                              : { height: dynamicHeight, width: dynamicWidth }
-                          }
-                        />
-                        {!image.imgDesc ? null : (
-                          <p className="image-description">{image.imgDesc}</p>
-                        )}
-                      </li>
-                    )
-                  })}
-                </ul>
+                      return (
+                        <li
+                          key={image.imgSrc.childImageSharp.id}
+                          style={{ width: wideMobileWidth }}
+                        >
+                          <Img
+                            fluid={image.imgSrc.childImageSharp.fluid}
+                            style={
+                              !image.imgDesc
+                                ? { height: dynamicHeight, width: "100%" }
+                                : { height: dynamicHeight, width: dynamicWidth }
+                            }
+                          />
+                          {!image.imgDesc ? null : (
+                            <p className="image-description">{image.imgDesc}</p>
+                          )}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
               </div>
-            </div>
-          )
-        })}
-      </HorizontalScroll>
-    </>
+            )
+          })}
+          {/* </HorizontalScroll> */}
+        </div>
+      </div>
+    </div>
   )
 }
 
